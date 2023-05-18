bfb02e4db98c2a054f153349eeb32e523795467b
firdevs19 <fiadhakeaf@gmail.com>
2023-05-18 23:50:12 +0200

    2023Mayis18

--- src/day01/C01.java
+++ src/day01/C01.java
@@ -4,9 +4,21 @@ public class C01 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
 
-        System.out.println("Merhaba Java");
-        System.out.println("deneme");
+        System.out.println("Break Statement\n");
+        for (int i = 1; i <= 5; i++) {
+
+            if (i == 4) break;
+            System.out.println(i);
+        }
+        System.out.println("Continue Statement\n");
+        for (int i = 0; i <= 5; i++) {
+            if (i == 1) continue;
+            System.out.println(i);
+        }
     }
+
 }
+
+
+
